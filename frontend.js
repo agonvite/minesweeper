a5cb7fe27fb1f2f102f4d502cb3c22f2c5f84a63
Ben Tattersley <ubershinysheep@gmail.com>
2013-09-15 13:53:42 +0100

    Basic UI added, renders a representation of the board

--- frontend.js
+++ frontend.js
@@ -1,3 +1,55 @@
 (function() {
-	console.log(backend.init(3,3));
+	
+	var frontend = {
+		
+		width: 0,
+		height: 0,
+		state: [],
+		
+		init: function (w,h,m) {
+			this.width = w;
+			this.height = h;
+			console.log(this.state = backend.init(w,h,m));
+			this.buildBoard();
+		},
+		
+		buildBoard: function () {
+			var d = document.createDocumentFragment(),
+				t = document.createElement('table'),
+				r = document.createElement('tr'),
+				c = document.createElement('td'),
+				w = this.width,
+				h = this.height,
+				s = this.state,
+				i = 0,
+				j = 0,
+				currentRow,
+				currentCell;
+			c.innerHTML = '&nbsp;';
+			d.appendChild(t);
+			
+			for(i;i<h;i++) {
+				
+				currentRow = r.cloneNode();
+
+				for(j=0;j<w;j++) {
+					currentCell = c.cloneNode();
+					currentCell.className = s[i][j].type;
+					currentRow.appendChild(currentCell);
+				}
+				
+				t.appendChild(currentRow);
+				
+			}
+			
+			document.body.appendChild(d);
+			
+		}
+		
+	}
+	
+	window.onload = function () {
+		frontend.init(3,3,Math.random()*0.5+0.25);
+	}
+	
 })()
