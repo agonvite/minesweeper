2688844935d4e9d6784e00998d2a1133656628a9
Ben Tattersley <ubershinysheep@gmail.com>
2013-09-15 12:50:36 +0100

    Fixed backend JS error, setup frontend

---
+++ frontend.js
@@ -0,0 +1,3 @@
+(function() {
+	console.log(backend.init(3,3));
+})()
--- mines.js
+++ mines.js
@@ -4,10 +4,10 @@ var backend = {
 			   i = 0,
 		       a = new Array(l),
 		 	cell = {
-				type: 'empty', //mine, empty
-				state: 'initial' //hidden, numbered, flagged, blank
-				number: null, //[1-9]
-				flagged: false //true, false
+				type: 'empty', 		//mine, empty
+				state: 'initial', 	//hidden, numbered, flagged, blank
+				number: null, 		//[1-9]
+				flagged: false 		//true, false
 			};
 		for(i;i<l;i++){
 			a[i] = cell;
